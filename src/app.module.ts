001a54165b869cd9efa1d3c0b4061982becc2749
Santiago Barrera <santiago.barrerae@gmail.com>
2022-11-30 09:00:35 -0500

    services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './modules/client/client.module';
+import { AccountModule } from './modules/account/account.module';
+import { SettingModule } from './modules/setting/setting.module';
+import { MovementModule } from './modules/movement/movement.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, AccountModule, SettingModule, MovementModule],
   controllers: [AppController],
   providers: [AppService],
 })
